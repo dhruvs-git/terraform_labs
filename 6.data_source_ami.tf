3f029fd9565b5def137d29119ef77f343cba7114
Dhruv Barot <dbarot3553@gmail.com>
2026-03-22 00:12:54 -0400

    data source added

---
+++ 6.data_source_ami.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ca-central-1"
+    profile = "terralearn"
+}
+
+data "aws_ami" "ami_fetch" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+    ami = data.aws_ami.ami_fetch.image_id
+    instance_type = "t3.micro"
+
+    tags = {
+      Name = "ec2-1"
+    }
+}
